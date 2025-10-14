9d7431fa3a2004378aad425fa4ceba1af4dc467a
Roger Monterroso <rogerclad228@gmail.com>
2025-10-13 23:15:39 -0600

    ahora si, arregle el route dinamico

--- src/app/api/[id]/route.ts
+++ src/app/api/[id]/route.ts
@@ -1,10 +1,13 @@
+import PostgresRepositorySelector from "@/app/utils/postgres-repository-selector";
 import PostgresUserRepository from "@/app/utils/postgres-user-repository";
 import UserFinder from "@/app/utils/user-finder";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest, { params }: { params: { id: string } }){
     try{
-        const repository = new PostgresUserRepository;
+        const selector = new PostgresRepositorySelector();
+        
+        const repository = await selector.getRepository();
 
         const finder = new UserFinder(repository);
 
--- src/app/utils/postgres-repository-selector.ts
+++ src/app/utils/postgres-repository-selector.ts
@@ -2,6 +2,7 @@ import PostgresUserRepository from "./postgres-user-repository";
 
 export default class PostgresRepositorySelector {
     constructor(){}
+    
     public async getRepository(){
         const repository = new PostgresUserRepository();
         return repository;
