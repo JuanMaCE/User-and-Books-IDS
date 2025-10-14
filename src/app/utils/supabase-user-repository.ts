3657832616c505bbce96a825988c20fbce0afaf5
JuanMaCE <jk120365@gmail.com>
2025-10-13 20:31:35 -0600

    roger is tonto x3

--- src/app/utils/supabase-user-repository.ts
+++ src/app/utils/supabase-user-repository.ts
@@ -2,17 +2,16 @@ import postgres, { Sql } from "postgres";
 import { User } from "./user";
 import UserRepository from "./user-repository";
 
-export default class SupabaseUserRepository implements UserRepository{
+export default class SupabaseUserRepository implements UserRepository {
     private readonly sql: Sql;
     constructor() {
-        const connectionString = 'postgresql://postgres.nviffybhwodpqzwumhtx:IbanezGio28*@aws-0-us-east-1.pooler.supabase.com:6543/postgres'
+        const connectionString = ''
         this.sql = postgres(connectionString);
     }
 
-    async save(user: User){
-        try{
+    async save(user: User) {
+        try {
             const userdto = user.toPrimitives();
-            const id = Number(userdto.id);
             const email = userdto.email;
             const dpi = userdto.dpi;
             const name = userdto.name;
@@ -21,15 +20,14 @@ export default class SupabaseUserRepository implements UserRepository{
             const isValidBool = isValid.toLowerCase() === "true";
 
 
-
-            await this.sql`INSERT INTO "Users" (id, email, dpi, name, age, isValid) VALUES (${id}, ${email}, ${dpi}, ${name}, ${age}, ${isValidBool})`;
-        }catch (err){
+            await this.sql`INSERT INTO "Users" (id, email, dpi, name, age, isValid) VALUES ( ${email}, ${dpi}, ${name}, ${age}, ${isValidBool})`;
+        } catch (err) {
             throw err
         }
     }
 
-    async find(id: string){
-        try{
+    async find(id: string) {
+        try {
             await this.sql`SELECT * FROM "Users" WHERE id=${id}`;
         } catch {
             throw new Error("User not found");
